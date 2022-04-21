6ed3b2206124f5dca6b79fb4fd329884880e729d
William Fincham <56725935+wilfinc@users.noreply.github.com>
2022-04-21 09:01:40 +0100

    removed obsolete code

--- inst/shinyApp/ukghg-app/app.R
+++ inst/shinyApp/ukghg-app/app.R
@@ -36,20 +36,6 @@
                    "Agriculture",
                    "Natural")
   
-  # ls_sectors <- 1:11
-  # sectorNames <- c("Energy production",
-  #                  "Domestic combustion",
-  #                  "Industrial combustion",
-  #                  "Industrial processes",
-  #                  "Offshore",
-  #                  "Solvents",
-  #                  "Road transport",
-  #                  "Other transport",
-  #                  "Waste",
-  #                  "Agriculture",
-  #                  "Natural")
-  # names(ls_sectors) <- sectorNames
-  
   # some inital values for selections
   df_init <- data.frame(
     startDate = "01/01/2014 00:00",
@@ -94,7 +80,6 @@
                        column(4, selectInput('select_gas', h5("Gas"), choices = as.list(c("CH4", "CO2", "N2O", "C2H6", "VOC")))),
                        column(4, selectInput('select_proj', h5("Projection"), choices = as.list(c("OSGB", "LonLat")))),
                        column(4, uiOutput('res_selector')),
-                       #column(4, selectInput('select_res', h5("Resolution"), choices = list('OSGB (km)' = as.list(c("1", "20", "100")),'LonLat (deg)' = as.list(c("0.1"))), selected = "100" ))
                        ), 
                      fluidRow(
                        column(4, selectInput("select_unitType", label = h5("Units"), choices = as.list(c("mol", "g")))),
@@ -126,43 +111,21 @@
                        column(width = 3.5, checkboxInput("log_wday", "Day-of-week", FALSE)),
                        column(width = 3, checkboxInput("log_hour", "Diurnal", FALSE)),
                        column(width = 12, checkboxInput("log_includeBio", "Include Natural Biogenic Fluxes", TRUE)), align="center"
-                       #column(3, actionButton("sendjobbutton", "Run Model", class="btn btn-outline-success btn-lg"), align="center")
                        )),
-                     # fluidRow(
-                     #   column(12, actionButton("sendjobbutton", "Run Model", class="btn btn-outline-success btn-lg"), align="center")
-                     # )),
                    mainPanel(
                      column(12, h3("Model parameters:"), align="center"),
-                     #leafletOutput('map', width = "100%", height = "100%"),  
-                     #plotOutput('timePlot'),
-                     #verbatimTextOutput('tmp'),
                      dataTableOutput('job_table')
                      )
-                   #mainPanel(leafletOutput('map')) # I'll add my leaflet map here
                  )),
         tabPanel("View Output", 
-                 #htmlOutput('waiting_text', class = 'waiting_text'),
                  leafletOutput('map', height = '795px'),
                  absolutePanel(
                    id = "map_controls", class = "panel panel-default", fixed = TRUE, draggable = TRUE, top = 200, left = 100, right = 'auto', bottom = "auto", width = 330, height = "auto",
-                   #h5('Select Sector'),
                    selectInput('sel_sector', label = 'Select sector:', choices = sectorNames),
-                   #selectInput('sel_sector', label = 'Select sector:', choices = c('Total', sectorNames)),
-                   #selectInput('sel_sector', label = 'Select sector:', choices = sectorNames),
                    uiOutput('time_points'),
                    column(12, actionButton("plotmapbutton", "Plot Data", class = "btn btn-success btn-lg"), align = 'center')
                    ),
-                 #selectInput('sel_sector', label = 'Select sector:', choices = sectorNames)
-                 #leafletOutput('map', height = '795px'),
         ),
-        tabPanel("Help",
-                 verbatimTextOutput('title_text'),
-                 verbatimTextOutput('checking_time_i'), 
-                 #verbatimTextOutput('checks'),
-                 plotOutput('checks'),
-                 verbatimTextOutput('title_text2'),
-                 ), 
-        
         tabPanel("Download",
                  verbatimTextOutput('raster_info'),
                  plotOutput('raster_check'),
@@ -199,13 +162,8 @@
       seq(startDate(), endDate(), length = input$intslider)
     })
     
-    #observeEvent(input$sendjobbutton, {
-      output$time_points <- renderUI(selectInput('timp_sector', label = 'Select timepoint:', choices = strftime(datect(), format = "%d-%m-%Y %H:%M")))
-    #})
-      
-    # time_point_i <- observeEvent(input$sendjobbutton, {  
-    #   which(input$timp_sector == strftime(datect(), format = "%d-%m-%Y %H:%M"))
-    #   })
+    output$time_points <- renderUI(selectInput('timp_sector', label = 'Select timepoint:', choices = strftime(datect(), format = "%d-%m-%Y %H:%M")))
+
       
     time_point_i <- reactive({which(input$timp_sector == strftime(datect(), format = "%d-%m-%Y %H:%M"))})
     
@@ -213,8 +171,6 @@
       # create a job dataframe
       tibble(datech = as.character(datect()),
                  ghgName = input$select_gas,
-                 #ghgName = tolower(input$select_gas),
-                 #nice_ghgName = input$select_gas,
                  proj = input$select_proj,
                  res = input$select_res,
                  unitType = input$select_unitType,
@@ -240,36 +196,17 @@
       ))
     }, serve = F)
   
-    # myFlux <- eventReactive(input$sendjobbutton, {
-    #   #shinybusy::show_modal_spinner()
-    #   #showModal(modalDialog('Model running'))
-    #   calcFlux(tolower(input$select_gas), job_df()$datect, proj = input$select_proj,
-    #                      res = gsub(' km| deg', '', input$select_res), input$select_unitType, input$select_unitSIprefix,
-    #                      includeBio = input$log_includeBio,
-    #                      timeScales = c(input$log_year, input$log_yday, input$log_wday, input$log_hour),
-    #                      writeNetCDF = input$select_writeNetCDF)
-    #   #removeModal()
-    #   #shinybusy::remove_modal_spinner()
-    # })
-    
     observeEvent(input$sendjobbutton, {
       shinybusy::show_modal_spinner()
-      #showModal(modalDialog('Model running'))
       myFlux <<- calcFlux(tolower(input$select_gas), job_df()$datect, proj = input$select_proj,
                          res = gsub(' km| deg', '', input$select_res), input$select_unitType, input$select_unitSIprefix,
                          includeBio = input$log_includeBio,
                          timeScales = c(input$log_year, input$log_yday, input$log_wday, input$log_hour),
                          writeNetCDF = input$select_writeNetCDF)
-      #removeModal()
       shinybusy::remove_modal_spinner()
     })
 
-    # output$timePlot <- renderPlot({
-    #         ylab_text <- paste("Total Net Flux, Tg", input$select_gas, "/y")
-    #         plot(job_df()$datect, myFlux$total, type = "b", xlab = "Date", ylab = ylab_text)
-    #       })
-    
-    #reactive()
+   
     
     target_stack <- reactive({
       if(input$sel_sector == 'Total'){
@@ -278,23 +215,10 @@
         myFlux$ls_ghgBySectorByTime[[which(input$sel_sector == sectorNames)-1]][[time_point_i()]]
       }
     })
-     
-    #target_rast <- reactive({target_stack()[[time_point_i()]]})
-    
-    # #target_stack_n <- reactive({dim(target_stack())[3]}) # extract the depth of the raster stack - i.e. how many time points? 
-     
+
     vals <- reactive({values(target_stack())})
-    #vals <- reactive({values(myFlux$s_ghgTotal[[1]])})
-    output$title_text <- renderPrint(vals())
-    #output$title_text <- reactive(paste(input$sel_sector, input$timp_sector, time_point_i(), sep = ' '))
-    output$checks <- renderPlot(plot(target_stack()[[time_point_i()]]))
-    #output$title_text2 <- renderPrint(vals2())
-    #output$checks <- renderText(dim(target_stack()[[time_point_i()]]))
-    #output$checks <- renderText(names(target_stack()))
-  
-    #####
+
     observeEvent(input$sendjobbutton, {
-      #shinybusy::show_modal_spinner()
       output$map <- renderLeaflet({
         leaflet() %>%
         addTiles() %>%
@@ -314,27 +238,6 @@
                  addLegend_decreasing(pal = pal, values = vals(), decreasing = T, title = toupper(input$select_gas))
     })
 
-    
-    #####
-    
-    # # THIS WORKED
-    # observeEvent(input$sendjobbutton, {
-    #     pal <- colorNumeric(palette = "viridis", domain = rev(vals()), na.color = "transparent", reverse = F)
-    # 
-    #     output$map <- renderLeaflet({
-    #       leaflet() %>%
-    #         addTiles() %>%
-    #         fitBounds(-7.57216793459, 49.959999905, 1.68153079591, 58.6350001085) %>%
-    #         addRasterImage(myFlux$s_ghgTotal[[1]], opacity = 0.5, colors = pal) %>%
-    #         addLegend_decreasing(pal = pal, values = vals(), decreasing = T, title = toupper(input$select_gas))
-    #       })
-    #     })
-  
-    
-    #target_raster <- observeEvent(input$sendjobbutton, myFlux$ls_ghgBySectorByTime[[ls_sectors[input$sel_sector]]], ignoreInit = T)
-    
-    #output$raster_check <- renderPlot(plot(target_raster()))
-  
     observeEvent(input$sendjobbutton == 0, {
       output$waiting_text <- renderText('<b>Outputs will appear here after the model has been run.<b>')
       }, once = T)
@@ -342,13 +245,6 @@
     observeEvent(input$sendjobbutton == 1, {
       output$waiting_text <- NULL
     }, ignoreInit = T)
-    
-    # observeEvent(input$sendjobbutton, {
-    #   target_raster <- myFlux$ls_ghgBySectorByTime[[ls_sectors[input$sel_sector]]]# will [1] beocme the time point...? issue of how to link time point number with date/time
-    #   output$raster_check <- renderPlot(plot(target_raster))
-    #   output$raster_info <- renderText(cat(dim(target_raster)))
-    # }, ignoreInit = T)
-    # 
     
     observe({
       if (input$TID == "View Output" | input$TID == "Download" )  {
@@ -361,43 +257,6 @@
         }
       }
     })
-    
-    
-    #output$raster_check <- renderPlot(plot(target_raster))
-  
-    # eventReactive(input$sel_sector, {
-    #   #names(myFlux$ls_ghgBySectorByTime) <- sectorNames
-    #   leafletProxy('map') %>%
-    #     addRasterImage(target_raster)
-    #   })
-  
-    
-    
-    
-    
-    
-    
-    
-    # observeEvent(input$gender == "Female", {
-    #   leafletProxy("map", data=lnd) %>%
-    #     clearShapes() %>%
-    #     addPolygons(stroke = FALSE, smoothFactor = 0.2, fillOpacity = 1, popup = popup2)
-    # })
-  
-    # observe({
-    #   names(myFlux$ls_ghgBySectorByTime) <- sectorNames
-    # 
-    #   proxy <- leafletProxy('map')
-    # 
-    #   proxy %>%
-    #     addRasterImage(myFlux$ls_ghgBySectorByTime[1])
-    # 
-    # })
-  
-    
-    
-    
-    
     
   }
   
